a5a21f8b85722767c83ed6d463401770a4ea41b7
CoolShivas <shivas2710cool00@gmail.com>
2024-04-29 14:30:24 +0530

    SignUp_functionality_with_CrudCrud

--- authentication_project/src/components/Auth/AuthForm.jsx
+++ authentication_project/src/components/Auth/AuthForm.jsx
@@ -1,11 +1,43 @@
-import { useState } from "react";
+import { useState , useRef} from "react";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+
   const [isLogin, setIsLogin] = useState(true);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
+  };
+
+  const handlerOnSubmitForm = (event) =>{
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if(isLogin)
+    {
+
+    }
+    else{
+      fetch(`https://crudcrud.com/api/dfb5b7e301e2492eb241643d7093be68/bhaiyaJi`,{
+        method : 'POST',
+        body : JSON.stringify({
+          email : enteredEmail,
+          password : enteredPassword,
+          returnSecureToken : true,
+        }),
+        headers:{
+          'Content-Type' : 'application/json'
+        },
+      }).then((res)=>{
+        console.log(res);
+      }).catch((err)=>{
+        console.log(err);
+      })
+    }
   };
 
   return (
@@ -13,14 +45,14 @@ const AuthForm = () => {
       {/* <h1> Login / SignUp </h1> */}
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
 
-      <form>
+      <form onSubmit={handlerOnSubmitForm }>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input type="password" id="password" required ref={passwordInputRef}/>
         </div>
         <div className={classes.actions}>
           <button> {isLogin ? 'Login' : 'Create Account'} </button>
