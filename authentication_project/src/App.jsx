53c57327b9e8d4e181573784b5adf8f798f0a5f4
CoolShivas <shivas2710cool00@gmail.com>
2024-04-30 12:44:29 +0530

    Protecting-Frontend-pages with the help of Route and Redirect Conditionally

--- authentication_project/src/App.jsx
+++ authentication_project/src/App.jsx
@@ -1,22 +1,43 @@
+import { useContext } from "react";
 import Layout from "./components/Layout/Layout";
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from "./pages/HomePage";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AuthContext from "./store/Auth-Context";
 
 function App() {
+
+ const {isLoggedIn}  = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+
+        {/* Here, if the user is not logged in then no need to show the auth page i.e, logIn or signUp page  */}
+        {!isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>}
+
+        {/* Here, if we are logged in then only the profile url and userprofile page will appear */}
+        {/* {isLoggedIn && <Route path='/profile'>
           <UserProfile />
+        </Route>} */}
+        {/* Here, profile url acces is possible ; */}
+        <Route path="/profile">
+          {/* But, here if the user logged in then access the profile page or redirect to login or signup page */}
+          {isLoggedIn && <UserProfile />}
+          {!isLoggedIn && <Redirect to="/auth"></Redirect>}
         </Route>
+
+        {/* Here, if user without logged in will be redirect to home or enter any invalid url  */}
+        <Route path="*">
+          <Redirect to="/"></Redirect>
+        </Route>
+
       </Switch>
     </Layout>
   );
